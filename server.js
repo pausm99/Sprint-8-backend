03c64694d2c2010f6a9cad7abc04205fc452d0b7
pausm99 <pausm99@gmail.com>
2023-12-12 19:23:35 +0100

    FIX: enable CORS policy

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 require("dotenv").config(); // ALLOWS ENVIRONMENT VARIABLES TO BE SET ON PROCESS.ENV SHOULD BE AT TOP
 
 const express = require("express");
+const cors = require('cors');
 const app = express();
 
 // Middleware
-app.use(express.json()); // parse json bodies in the request object
+app.use(express.json(), cors()); // parse json bodies in the request object
 
 // Redirect requests to endpoint starting with /reservations to reservationRoutes.js
 app.use("/reservations", require("./routes/reservationRoutes"));
